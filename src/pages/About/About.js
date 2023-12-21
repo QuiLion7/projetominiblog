a4e264351c951fb660497892738e6fceee89d174
QuiLion7 <quilbrub@gmail.com>
2023-12-20 21:04:23 -0300

    Added more details about the project's technologies and goals

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,14 +1,23 @@
-import { Link } from 'react-router-dom'
-import styles from './About.module.css'
+import { Link } from "react-router-dom";
+import styles from "./About.module.css";
 
 const About = () => {
   return (
     <div className={styles.about}>
-      <h1>Sobre o Mini <span>Blog</span></h1>
-      <p>Projeto que utiliza Rect no front-end e Firebase no back-end</p>
-      <Link to='/posts/create' className='btn'>Criar Post</Link>
+      <h1>
+        Sobre o Mini <span>Blog</span>
+      </h1>
+      <p>
+        Este projeto é um blog simples que utiliza React no front-end e Firebase
+        no back-end. Ele foi desenvolvido por mim, com o objetivo de realizar
+        operações CRUD (create, read, update, delete), além de exibir as
+        informações cadastradas no banco de dados.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
